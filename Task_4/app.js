64a56cf057995cd761bfe68809bf8bc0c97b2a56
Poorva Wadar <wadarpoorva@gmail.com>
2024-02-03 00:54:26 +0530

    commit

--- Task_4/app.js
+++ Task_4/app.js
@@ -1,4 +1,4 @@
-// Common functions for both login and signup pages
+
 function redirectToLogin() {
     window.location.href = 'login.html';
 }
@@ -75,3 +75,97 @@ function submitSignup() {
     window.location.href = `index.html?username=${encodeURIComponent(newUsername)}`;
 }
 
+const videoCards = document.querySelectorAll('.video-card');
+
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play().catch(error => {
+            // Autoplay was prevented. Handle the error, e.g., by showing a play button.
+            console.error("Autoplay prevented: ", error);
+        });
+    });
+
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    });
+});
+
+
+// cards sliders
+
+let cardContainers = document.querySelectorAll('.card-container');
+let preBtns = document.querySelectorAll('.pre-btn');
+let nxtBtns = document.querySelectorAll('.nxt-btn');
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
+
+const movies = [
+    "Bhediya",
+    "Loki",
+    "Wanda Vision",
+    "Raya and the Last Dragon",
+    "Luca",
+    "Mrs.Chatterjee Vs Norway",
+    "Tu Jhooti Main Makkar",
+    "creater",
+    "The Mother",
+    "Moving",
+    "My demon",
+    "Advengers",
+    "Squid Game",
+    "Jawan",
+    "Pathaan",
+    "Animal",
+    "Gadar 2",
+    "The Kerala Story",
+    "The Outlaws 2",
+    "The Witch: Part 2",
+    "Seobok",
+    "Avatar 2",
+    "Guardians of the Galaxy Vol. 3",
+    "Fantastic Beasts and Where to Find Them 3"
+];
+
+const searchInput = document.getElementById('searchInput');
+const movieContainer = document.getElementById('movieContainer');
+
+searchInput.addEventListener('input', handleSearch);
+searchInput.addEventListener('keydown', handleKeyPress);
+
+function handleSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredMovies = movies.filter(movie => movie.toLowerCase().includes(searchTerm));
+    displayMovies(filteredMovies);
+}
+
+function displayMovies(moviesToShow) {
+    movieContainer.innerHTML = '';
+
+    moviesToShow.forEach(movie => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.textContent = movie;
+
+        movieContainer.appendChild(card);
+    });
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        handleSearch();
+        console.log('Hello');
+    }
+}
